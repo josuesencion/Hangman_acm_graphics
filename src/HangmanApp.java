606497094705562a1594c3d1de666544bf3aa58d
Josue Sencion <josue.sencion1@gmail.com>
2019-03-14 11:50:32 -0400

    2nd. Adding stick figure to canvas

--- src/HangmanApp.java
+++ src/HangmanApp.java
@@ -1,8 +1,4 @@
-import acm.graphics.*;
 import acm.program.*;
-import acm.util.*;
-
-import java.awt.*;
 
 public class HangmanApp extends ConsoleProgram {
 
@@ -14,27 +10,39 @@ public class HangmanApp extends ConsoleProgram {
     }
 
     public void run() {
-
+        setTitle("Hangman");
         println("Welcome to Hangman");
 
-        HangmanLexicon lexicon = new HangmanLexicon();
-        Hangman game = new Hangman(lexicon.getRandWord(), lexicon.getHint());
+        char playAgain;
 
-        while (!game.compareSecretWord() && game.getGuessesLeft() != 0) {
-            this.canvas.displayWord(game.getHint());
-            println(game.displayHint());
-            println(game.showGuessesLeft());
-            char guess = readLine("Your guess: ").toUpperCase().charAt(0);
-            println(game.verifyGuess(guess));
+        do {
             this.canvas.reset();
-        }
+            HangmanLexicon lexicon = new HangmanLexicon();
+            Hangman game = new Hangman(lexicon.getRandWord(), lexicon.getHint());
 
-        if (game.compareSecretWord()) {
-            println("You guessed the word: " + game.getSecretWord());
-        } else {
-            println("You lost. The word is: " + game.getSecretWord());
-            this.canvas.displayWord(game.getSecretWord());
-        }
+            while (!game.compareSecretWord() && game.getGuessesLeft() != 0) {
+                this.canvas.drawFigure(game.getGuessesLeft());
+                this.canvas.displayHint(game.getHint());
+                println(game.displayHint());
+                println(game.showGuessesLeft());
+                char guess = readLine("Your guess: ").toUpperCase().charAt(0);
+                println(game.verifyGuess(guess));
+                this.canvas.reset();
+            }
+
+            if (game.compareSecretWord()) {
+                println("You guessed the word: " + game.getSecretWord());
+            } else {
+                println("You lose! The word is: " + game.getSecretWord());
+                this.canvas.drawFigure(game.getGuessesLeft());
+                this.canvas.displayHint("You lose! The word is: ");
+                this.canvas.displaySecretWord(game.getSecretWord());
+            }
+
+            playAgain = readLine("Would you like to play again? Y/N: ").charAt(0);
+        } while (playAgain == 'Y' || playAgain == 'y');
+
+        println("Thank you for playing Hangman! Get lost, bitch!");
     }
 
     public static void main(String[] args) {
--- src/HangmanCanvas.java
+++ src/HangmanCanvas.java
@@ -1,47 +1,99 @@
 import acm.graphics.*;
 import acm.graphics.GLabel;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class HangmanCanvas extends GCanvas {
 
-    private static final int SCAFFOLD_HEIGHT = 360;
-    private static final int BEAM_LENGTH = 144;
-    private static final int ROPE_LENGTH = 18;
-    private static final int HEAD_RADIUS = 36;
-    private static final int BODY_LENGTH = 144;
-    private static final int ARM_OFFSET_FROM_HEAD = 28;
-    private static final int UPPER_ARM_LENGTH = 72;
-    private static final int LOWER_ARM_LENGTH = 44;
-    private static final int HIP_WIDTH = 36;
-    private static final int LEG_LENGTH = 108;
-    private static final int FOOT_LENGTH = 28;
+    private List<GObject> stickFigure;
+    private List<GObject> scaffolding;
 
-
+    public HangmanCanvas(){
+        this.stickFigure = creteStickFigure();
+        this.scaffolding = creteScaffolding();
+    }
 
     public void reset() {
         removeAll();
     }
 
-    public void displayWord(String word) {
+    public void displayHint(String word) {
         GLabel label = new GLabel(word);
         label.setFont("SansSerif-20");
         double x = (getWidth() - label.getWidth()) / 2;
-        double y = (getHeight() + label.getAscent()) / 1.5;
+        double y = (getHeight() + label.getAscent()) / 1.2;
         add(label, x, y);
-    }
-
-    public void drawStickFigure() {
 
     }
 
-    private List<GObject> creteStickFigure(){
-        GOval head = new GOval(10, 10, 72, 72);
+    public void displaySecretWord(String word) {
+        GLabel label = new GLabel(word);
+        label.setFont("SansSerif-20");
+        double x = (getWidth() - label.getWidth()) / 2;
+        double y = (getHeight() + label.getAscent()) / 1.1;
+        add(label, x, y);
 
-        double xHead = getWidth()/2;
-        GLine body = new GLine(xHead, 30,xHead, 90 );
+    }
 
+    public void drawFigure(int guess) {
+        this.drawScaffolding();
+        this.drawStickFigure(guess);
 
+    }
+
+    private void drawStickFigure(int guess) {
+        int numParts = 8 - guess;
+        for (int i = 0; i < numParts; i++) {
+            add(this.stickFigure.get(i));
+        }
+    }
+
+    private void drawScaffolding() {
+        for (GObject part :
+        this.scaffolding) {
+            add(part);
+        }
+
+    }
+
+    private List<GObject> creteStickFigure() {
+        List<GObject> stickFigure = new ArrayList<>();
+
+        //GOval head = new GOval(164, 80, 50, 50);
+        GImage head = new GImage("felix-el-ladron-bautista.jpg",164,45);
+        GLine body = new GLine(189, 130, 189, 230);
+        GLine leftArm = new GLine(189, 130, 140, 180);
+        GLine rightArm = new GLine(189, 130, 238, 180);
+        GLine leftLeg = new GLine(189, 230, 140, 300);
+        GLine rightLeg = new GLine(189, 230, 238, 300);
+        GLine leftFoot = new GLine(139, 300, 119, 300);
+        GLine rightFoot = new GLine(238, 300, 258, 300);
+
+        stickFigure.add(head);
+        stickFigure.add(body);
+        stickFigure.add(leftArm);
+        stickFigure.add(rightArm);
+        stickFigure.add(leftLeg);
+        stickFigure.add(rightLeg);
+        stickFigure.add(leftFoot);
+        stickFigure.add(rightFoot);
+
+        return stickFigure;
+    }
+
+    private List<GObject> creteScaffolding(){
+        List<GObject> scaffolding = new ArrayList<>();
+
+        GLine part1 = new GLine(50, 30,50, 350);
+        GLine part2 = new GLine(50, 30,189, 30);
+        GLine rope = new GLine(189, 30,189, 80);
+
+        scaffolding.add(part1);
+        scaffolding.add(part2);
+        scaffolding.add(rope);
+
+        return scaffolding;
     }
 
 }
